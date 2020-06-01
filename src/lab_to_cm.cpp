af5d9f12f84f41525ef2b59318c8b96268ad8b51
mam-mih-val <mam.mih.val@gmail.com>
2020-06-01 22:07:58 +0300

    Initial commit

---
+++ src/lab_to_cm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+int main(int n_args, char** args) {
+  if( n_args != 2) {
+	std::cout << "Error: lack of arguments. 1 argument required" << std::endl;
+	return 1;
+  }
+  const double M = 0.938;
+  double T = std::atof(args[1]);
+  const double GAMMA = (T + M) / M;
+  const double BETA = sqrt(1 - (M * M) / (M + T) / (M + T));
+  const double PZ = M * BETA * GAMMA;
+  const double E = T + M;
+  const double Y_BEAM = 0.5 * log((E + PZ) / (E - PZ));
+  double sqrt_s = sqrt( 2*M*M + 2*M*E );
+  const double GAMMA_CMS = sqrt_s/2/M;
+  std::cout << "T\t\t\tsqrt(S)\t\ty_beam/2\t\tp_cms\t\tgamma_beam" << std::endl;
+  std::cout << T << "\t\t" << sqrt_s << "\t\t" << Y_BEAM/2 << "\t\t" << E*BETA << "\t\t" << GAMMA_CMS << std::endl;
+  std::cout << "AGeV\t\tGeV\t\t\t\t\t\t\tGeV/c" << std::endl;
+  return 0;
+}
